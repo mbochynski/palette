847af63c369ff8e09e9928b3067c1e7e84b5a99b
mbochynski <mbochynski@MacBook-Pro-mbochynski.local>
2015-09-09 23:06:51 +0200

    get color of picture

--- spikes/raphael/colorPicker.js
+++ spikes/raphael/colorPicker.js
@@ -5,17 +5,26 @@ var paper_dim = {
 
 var image_url = 'images/1.jpg';
 
-var div = document.getElementById('image');
+var div = document.getElementById('raphael_image');
 
 var paper = Raphael(div, paper_dim.width, paper_dim.height);
 
 var r_image = paper.image(image_url, 0, 0, paper_dim.width, paper_dim.height);
 
-var image = new Image(paper_dim.width, paper_dim.height);
-image.src = image_url;
+//var image = new Image(paper_dim.width, paper_dim.height);
+//image.src = image_url;
 
-div.appendChild(image);
+//div.appendChild(image);
+
+var image = document.getElementById('image');
+var canvas = document.createElement('canvas');
+canvas.width = paper_dim.width;
+canvas.height = paper_dim.height;
+var context = canvas.getContext('2d');
+context.drawImage(image, 0, 0, paper_dim.width, paper_dim.height);
+var data;
 
 r_image.mousemove(function(ev){
-  
+  data = context.getImageData( ev.layerX, ev.layerY, 1, 1).data;
+  console.log( data[0], data[1], data[2], data[3] );
 });
