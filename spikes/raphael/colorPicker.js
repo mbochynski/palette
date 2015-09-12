2357c5d25d51c1d681c91faa667ae96f5513f56a
mbochynski <mbochynski@MacBook-Pro-mbochynski.local>
2015-09-12 19:33:05 +0200

    moving enlarged pixel, changing its color, custom cursor

--- spikes/raphael/colorPicker.js
+++ spikes/raphael/colorPicker.js
@@ -23,19 +23,28 @@ area.attr({
   
 });
 
-var zoomed = paper.rect(0, 0, 1, 1);
+var zoomed = paper.rect(0, 0, 10, 10);
 zoomed.attr({
   "stroke-width": 0,
-  "fill": '#fff',
-  "fill-opacity": '0',
+  "fill": '#00f',
+  "fill-opacity": '1',
 });
+
+function moveElementWithColor(el, x, y, color_arr){
+  el.attr({
+    x: x+10,
+    y: y+10,
+    fill: Raphael.rgb(color_arr[0], color_arr[1], color_arr[2])
+  });
+}
 
 var x, y;
 area.mousemove(function(ev){
   x = ev.layerX;
   y = ev.layerY;
-  data = context.getImageData( x, y, 1, 1).data;
-  console.log( data[0], data[1], data[2], data[3] );
+  data = context.getImageData(x, y, 1, 1).data;
+  moveElementWithColor(zoomed, x, y, data);
+  console.log(data[0], data[1], data[2], data[3]);
 });
 
 function usuned() {
